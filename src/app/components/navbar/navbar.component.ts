427feba9d7eb0e8190087f6b30531214fe14477d
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-06-12 10:22:15 -0500

    navbar component fixed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   isNavbarCollapsed: boolean = true;
 
-  rutas: Object[] = [
+  rutas = [
     {
       name: 'Home',
       path: '/home',
